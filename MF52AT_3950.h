03f3480f9de70f0ed4169a63f7898d710a571d79
Yuriy Kapoyko <yury.kapoyko@gmail.com>
2024-08-30 23:30:29 +0300

    Code written

---
+++ MF52AT_3950.h
@@ -0,0 +1,19 @@
+#ifndef MF52AT_3950_H
+#define MF52AT_3950_H
+
+#include <stdint.h>
+
+#define MF52AT_3950_TMIN -55
+#define MF52AT_3950_TMAX 125
+#define MF52AT_3950_TSTEP 1
+
+#define MF52AT_3950_TCOUNT ((MF52AT_3950_TMAX - MF52AT_3950_TMIN) / MF52AT_3950_TSTEP + 1)
+
+extern uint32_t mf52at_3950_100k_r[MF52AT_3950_TCOUNT];
+
+int MF52AT_3950_100k_GetTemp(int r);
+int MF52AT_3950_GetTemp(int r, int r25_kohm);
+
+const char *MF52AT_UnitTest(void);
+
+#endif // MF52AT_3950_H
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MF52AT
+
 Platform-agnostic MF52AT NTC library for embedded
+
+## Author
+
+Yuriy Kapoyko - ykapoyko@vk.com
